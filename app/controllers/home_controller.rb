d525829b9e78943089ee4901b0bafae367a36780
damidurodola <oluwadamilola.durodola@andela.com>
2022-02-07 10:20:29 +0100

    implement index route

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  skip_before_action :authorize_request
+
+  def index;end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :courses
 
+  get '/', to: 'home#index'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   post 'courses/:id/enroll', to: 'courses#enroll'
